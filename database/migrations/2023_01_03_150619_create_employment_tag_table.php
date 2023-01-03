6ce457a5e877a7b6ab995c981e644c186545f07d
Stefan <stefan.fraenkel@gmail.com>
2023-01-03 17:06:02 +0100

    create db migrations

---
+++ database/migrations/2023_01_03_150619_create_employment_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employment_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employment_id')->constrained('employments');
+            $table->foreignId('tag_id')->constrained('tags');$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employment_tag');
+    }
+};
