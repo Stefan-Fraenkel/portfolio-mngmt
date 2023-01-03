5631a1f89167a0683305e5ae94223b47b4341f9d
Stefan <stefan.fraenkel@gmail.com>
2023-01-03 13:55:44 +0100

    Update web.php
    setup initial routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('solid-state.index');
+})->name('home');
+
+Route::get('/generic', function () {
+    return view('solid-state.generic');
+})->name('generic');
+
+Route::get('/elements', function () {
+    return view('solid-state.elements');
+})->name('elements');
