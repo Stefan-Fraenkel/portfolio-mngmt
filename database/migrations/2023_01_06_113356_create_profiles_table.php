c74b498214890305f4c2d5c2848208201faf008f
Stefan <stefan.fraenkel@gmail.com>
2023-01-06 16:12:57 +0100

    Update 2023_01_06_113356_create_profiles_table.php
    add description field

--- database/migrations/2023_01_06_113356_create_profiles_table.php
+++ database/migrations/2023_01_06_113356_create_profiles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('linkedin')->nullable();
