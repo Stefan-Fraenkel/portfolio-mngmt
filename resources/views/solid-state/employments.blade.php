a12472aa31ca6fcd16d2404f56c6bb448e5ce90a
Stefan <stefan.fraenkel@gmail.com>
2023-01-22 23:52:26 +0100

    Update employments.blade.php

--- resources/views/solid-state/employments.blade.php
+++ resources/views/solid-state/employments.blade.php
@@ -44,55 +44,88 @@
 					<section id="wrapper">
 						<header>
 							<div class="inner">
-								<h2>Berufliche Erfahrungen</h2>
-								<p>Phasellus non pulvinar erat. Fusce tincidunt nisl eget ipsum.</p>
+								<h2>Berufliche Erfahrung</h2>
+								<p>Meine bisherigen Tätigkeiten.</p>
 							</div>
 						</header>
 
-						<!-- Content -->
-							<div class="wrapper">
-								<div class="inner">
-                                    @isset($employments)
-                                        @foreach($employments as $employment)
-                                            <section id="one" class="wrapper spotlight style1">
-                                                <div class="inner">
-                                                    <a href="{{$employment->url}}" class="image" target="_blank"><img src="{{ asset('/images/forsberg_logo_small.jpeg')}}" alt="" /></a>
-                                                    <div class="content">
-                                                        <h2 class="major">{{$employment->name}}</h2>
-                                                        <p>{!!$employment->short_description!!}</p>
-                                                        <a href="{{route('employments')}}" class="special"  target="_blank">mehr erfahren</a>
-                                                    </div>
+                        @foreach($employments as $number => $employment)
+                            @php
+                                $number = $number+1;
+                                $style = $number;
+                                while ($style > 5) // styles count up to style6
+                                    {
+                                        $style = $style - 5;
+                                    }
+                            @endphp
+                            @if($number % 2 == 0)
+                                <section id="{{$number}}" class="wrapper alt spotlight style{{$style}}">
+                            @else
+                                <section id="{{$number}}" class="wrapper spotlight style{{$style}}">
+                            @endif
+                                <div class="inner">
+                                    <a href="{{$employment->url}}" class="image" target="_blank"><img src="data:image/png;base64,{{$employment->image}}" alt="" /></a>
+                                    <div class="content">
+                                        <h2 class="major">
+                                            {{$employment->name}}
+                                        </h2>
+                                        @if($number % 2 == 0)
+                                            <div class="row" style="float: right">
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->employer}}</h4>
                                                 </div>
-                                            </section>
-                                        @endforeach
-                                    @endisset
-									<h3 class="major">Lorem ipsum dolor</h3>
-									<p>Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor. Duis vulputate ex et ex tincidunt, quis lacinia velit aliquet. Duis non efficitur nisi, id malesuada justo. Maecenas sagittis felis ac sagittis semper. Curabitur purus leo donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Etiam ut gravida sapien.</p>
-
-									<p>Vestibulum ultrices risus velit, sit amet blandit massa auctor sit amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis id vel diam. Praesent sit amet posuere risus, eu faucibus lectus. Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam. Proin nec commodo, vel scelerisque nisi scelerisque. Suspendisse id quam vel tortor tincidunt suscipit. Nullam auctor orci eu dolor consectetur, interdum ullamcorper ante tincidunt. Mauris felis nec felis elementum varius.</p>
-
-									<h3 class="major">Vitae phasellus</h3>
-									<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
-
-									<section class="features">
-										<article>
-											<a href="#" class="image"><img src="{{ asset('/images/pic04.jpg')}}" alt="" /></a>
-											<h3 class="major">Sed feugiat lorem</h3>
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-											<a href="#" class="special">Learn more</a>
-										</article>
-										<article>
-											<a href="#" class="image"><img src="{{ asset('/images/pic05.jpg')}}" alt="" /></a>
-											<h3 class="major">Nisl placerat</h3>
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-											<a href="#" class="special">Learn more</a>
-										</article>
-									</section>
-
-								</div>
-							</div>
-
-					</section>
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->location}}</h4>
+                                                </div>
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->from}} - {{$employment->to}}</h4>
+                                                </div>
+                                            </div>
+                                        @else
+                                            <div class="row" style="float: left">
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->employer}}</h4>
+                                                </div>
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->location}}</h4>
+                                                </div>
+                                                <div class="col-4-medium col-12-small">
+                                                    <h4>{{$employment->from}} - {{$employment->to}}</h4>
+                                                </div>
+                                            </div>
+                                        @endif
+                                        <br><br>
+                                        <div class="row">
+                                            @if($number % 2 == 0)
+                                                <div class="col-6 col-12-medium">
+                                                </div>
+                                                <div class="col-6 col-12-medium">
+                                                    <h4>Aufgaben</h4>
+                                                    <ul class="alt">
+                                                        @foreach($employment->short_descriptions as $short_description)
+                                                            <li>{{$short_description}}</li>
+                                                        @endforeach
+                                                    </ul>
+                                                </div>
+                                            @else
+                                                <div class="col-6 col-12-medium">
+                                                    <h4>Aufgaben</h4>
+                                                    <ul class="alt">
+                                                        @foreach($employment->short_descriptions as $short_description)
+                                                            <li>{{$short_description}}</li>
+                                                        @endforeach
+                                                    </ul>
+                                                </div>
+                                                <div class="col-6 col-12-medium">
+                                                </div>
+                                            @endif
+                                        </div>
+                                        <a href="{{route('employments')}}" class="special"  target="_blank">mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </section>
+                        @endforeach
+                    </section>
 
 				<!-- Footer -->
                 <section id="footer">
